627898cd46ddda38348b156005f9dcf23be2f791
Elif Damla Pinar <damlapnar1@gmail.com>
2026-04-12 09:00:00 -0400

    Initial Appium setup with thread-safe driver management

---
+++ src/main/java/com/automation/config/DriverManager.java
@@ -0,0 +1,60 @@
+package com.automation.config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class DriverManager {
+
+    private static final ThreadLocal<AppiumDriver> driverThread = new ThreadLocal<>();
+
+    public static AppiumDriver getDriver() {
+        return driverThread.get();
+    }
+
+    public static void initAndroidDriver() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("appium:deviceName", System.getProperty("deviceName", "emulator-5554"));
+        caps.setCapability("appium:platformVersion", System.getProperty("platformVersion", "14.0"));
+        caps.setCapability("appium:automationName", "UiAutomator2");
+        caps.setCapability("appium:app", System.getProperty("appPath", "apps/android/app-debug.apk"));
+        caps.setCapability("appium:noReset", false);
+
+        AndroidDriver driver = new AndroidDriver(
+            new URL("http://127.0.0.1:4723"),
+            caps
+        );
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driverThread.set(driver);
+    }
+
+    public static void initIOSDriver() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("appium:deviceName", System.getProperty("deviceName", "iPhone 15"));
+        caps.setCapability("appium:platformVersion", System.getProperty("platformVersion", "17.0"));
+        caps.setCapability("appium:automationName", "XCUITest");
+        caps.setCapability("appium:app", System.getProperty("appPath", "apps/ios/app.app"));
+        caps.setCapability("appium:noReset", false);
+
+        IOSDriver driver = new IOSDriver(
+            new URL("http://127.0.0.1:4723"),
+            caps
+        );
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driverThread.set(driver);
+    }
+
+    public static void quitDriver() {
+        if (driverThread.get() != null) {
+            driverThread.get().quit();
+            driverThread.remove();
+        }
+    }
+}
